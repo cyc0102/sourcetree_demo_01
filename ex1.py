31e2213a702f3e2f5fd16923c5e5b761af2494c3
Bryan <tw_bryan_chen@intretech.com>
2021-02-19 15:05:01 +0800

    first commit

---
+++ ex1.py
@@ -0,0 +1,10 @@
+# first test ok
+def f(n):
+    answer=1
+    for i in range(1,n+1):
+        answer *= i
+    return answer
+print (f(5))
+# modify for branch01
+# test ok for branch01
+# update file
